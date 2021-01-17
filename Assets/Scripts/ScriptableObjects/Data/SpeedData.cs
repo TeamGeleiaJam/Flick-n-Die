3d50cb8d11addf4bf1dbf036cab4e052196e127b
Daniel-Brum <danielphillipebrum@hotmail.com>
2021-01-17 18:54:29 -0300

    Header changes as proposed by Paulo

--- Assets/Scripts/ScriptableObjects/Data/SpeedData.cs
+++ Assets/Scripts/ScriptableObjects/Data/SpeedData.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(filename = "Speed Data", menuname = "Speed Data"), order = 0]
+[CreateAssetMenu(filename = "Speed Data", menuname = "Entity/Speed Data"), order = 0]
 public class SpeedData : ScriptableObject 
 {
     #region Field Declarations
