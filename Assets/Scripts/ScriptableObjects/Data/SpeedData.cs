bc4e949baba10d2ee7cbcf71f9892843a2a987d8
Daniel-Brum <danielphillipebrum@hotmail.com>
2021-01-17 18:31:04 -0300

    Initial version of HurtData.cs, FlickData.cs and SpeedData.cs. Review pending.

---
+++ Assets/Scripts/ScriptableObjects/Data/SpeedData.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(filename = "Speed Data", menuname = "Speed Data"), order = 0]
+public class SpeedData : ScriptableObject 
+{
+    #region Field Declarations
+    [Header("Speed Parameters")]
+    [Tooltip("The speed property.")]
+    [SerializeField] private float speed;
+    
+    public float Speed {get => speed;}
+    #endregion
+}
