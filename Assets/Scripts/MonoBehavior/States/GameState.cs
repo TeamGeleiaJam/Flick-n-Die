cea115dae2a9fae1f9c52f9f048da293bca333f3
DenysGrossi922 <64700127+DenysGrossi922@users.noreply.github.com>
2021-01-17 14:57:55 -0300

    Máquina de estados parcialmente criada

---
+++ Assets/Scripts/MonoBehavior/States/GameState.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : BaseGameState
+{
+    private GameObject playerTop;
+    private GameObject playerBot;
+    private GameObject[] spawnPoints;
+    private GameObject[] objectPrefabs;
+
+    // Start is called before the first frame update
+    private void GameOver()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void SpawnObjects()
+    {
+        
+    }
+}
