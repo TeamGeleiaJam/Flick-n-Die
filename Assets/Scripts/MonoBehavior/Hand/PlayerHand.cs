a09db5f0c116257e3c7e83910bf219b9457d4afe
DenysGrossi922 <64700127+DenysGrossi922@users.noreply.github.com>
2021-01-18 17:40:10 -0300

    PlayerHand Started

---
+++ Assets/Scripts/MonoBehavior/Hand/PlayerHand.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHand : MonoBehaviour
+{
+    private StatusEffectController statusEffectController { get; set; }
+    private HurtController hurtController { get; set; }
+
+    [SerializeField]
+    private MotorController motorController;
+    private FlickController flickController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
