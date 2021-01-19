008df25ef690c42367b344eab1031cf70ea1a778
Daniel-Brum <danielphillipebrum@hotmail.com>
2021-01-19 19:20:02 -0300

    Fixes on EEffectType.cs

--- Assets/Scripts/MonoBehavior/EEffectType.cs
+++ Assets/Scripts/MonoBehavior/EEffectType.cs
@@ -3,13 +3,7 @@ using System.Collections.Generic;
 
 public enum EEffectType 
 {
-	"BlackHole", 
-	"Bomb", 
-	"Coin", 
-    "DamageOverTime", 
-    "DamageReduction", 
-    "DamageToSelf", 
-    "Heal", 
-    "SlipperyTrail", 
-    "Slowness"
+    "DamageModifier", 
+    "Slow", 
+    "Weaken"
 }
