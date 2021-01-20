5a88384bf13ceaa0ec42cdc71396d794d92b9a33
Paulo H <paulo@ninito.me>
2021-01-20 02:49:17 -0300

    Fix minor errors on ItemBase, minor changes
    Add object despawn event to Pool method and Eventbroker.

--- Assets/Scripts/MonoBehavior/ItemBase.cs
+++ Assets/Scripts/MonoBehavior/ItemBase.cs
@@ -9,50 +9,61 @@ public class ItemBase : MonoBehaviour, IPoolable
     [SerializeField] private EffectController effectController;
     [SerializeField] private ItemData itemData;
     [SerializeField] private float lifetime;
-    
+
+    private float lifeTimer;
+    private ObjectPool objectPool;
+
     public EffectController EffectController {get => effectController;}
     public ItemData ItemData {get => itemData;}
+    public ObjectPool ObjectPool { get => objectPool; set => objectPool = value; }
+    public EItemType ItemType { get => itemType; set => itemType = value; }
     #endregion
-    
+
     #region Interface Implementations
     public void EnablePoolable() 
     {
-    	
+
     }
+    
     public void Pool()
     {
-    	
+        // Notifies the object has despawned
+        EventBroker.CallObjectDespawned();
     }
     #endregion
     
     #region Custom Methods
     public void OnHit() 
     {
-    	
+
     }
+    
     public void OnActivate() 
     {
-    	
+
     }
-    public void OnHitTarget(HurtController hurtController, StatusEffectController statusEffectController) 
-    {
+    
+    // public void OnHitTarget(HurtController hurtController, StatusEffectController statusEffectController) 
+    // {
     	
-    }
+    // }
     #endregion
     
     #region Coroutines
     private IEnumerator LifetimeRoutine() 
     {
-    	yield return new WaitForSeconds(lifetime);
-    	Pool();
-    	if (ObjectPoolManager.objectPools.ContainsKey(itemType)) 
+        yield return new WaitForSeconds(lifetime);
+
+        Pool();
+        
+    	if (ObjectPoolManager.Instance.ObjectPools.ContainsKey(itemType)) 
     	{
             ObjectPool newItemPool = new ObjectPool();
-            ObjectPoolManager.CreatePool(itemType, newItemPool);
+            ObjectPoolManager.Instance.CreatePool(itemType, newItemPool);
     	}
     	else 
     	{
-    		ObjectPoolManager.objectPools[itemType].ReturnObjectToPool(this.gameObject);
+    		ObjectPoolManager.Instance.ObjectPools[itemType].ReturnObjectToPool(this.gameObject);
     	}
     }
     #endregion
